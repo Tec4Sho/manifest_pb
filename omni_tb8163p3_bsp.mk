7affff0cd17fa8f4da2b7606e3de69e2dec43910
Ziona <143420949+Tec4Sho@users.noreply.github.com>
2025-09-19 17:45:28 -0400

    Update and rename omni_device.mk.sample.md to omni_tb8163p3_bsp.mk

--- omni_tb8163p3_bsp.mk
+++ omni_tb8163p3_bsp.mk
@@ -1,23 +1,19 @@
-<a href="README.md" >Go Back</a>
 
-### omni_device.mk sample
-
-```bash
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk) -- only for 64bit phones
 
 # Inherit from device
-$(call inherit-product, device/<path>/device.mk) -- path to main device makefile
+$(call inherit-product, device/alps/tb8163p3_bsp/device.mk) -- path to main device makefile
 
 # Inherit common product files.
 $(call inherit-product, vendor/pb/config/common.mk)
 
 # Set those variables here to overwrite the inherited values.
-BOARD_VENDOR := 
-PRODUCT_BRAND := 
-PRODUCT_DEVICE := 
-PRODUCT_NAME := omni_device
-PRODUCT_MANUFACTURER := 
-PRODUCT_MODEL := 
-TARGET_VENDOR := 
-```
+BOARD_VENDOR := tb8163p3_bsp
+TARGET_VENDOR := tb8163p3_bsp
+
+PRODUCT_DEVICE := tb8163p3_bsp
+PRODUCT_NAME := omni_tb8163p3_bsp
+PRODUCT_BRAND := alps
+PRODUCT_MODEL := tb8163p3_bsp
+PRODUCT_MANUFACTURER := alps
